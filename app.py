ccb2fc4cafce6c01d183e1bdfb22e0d433205a62
krishv2 <61357583+krishv2@users.noreply.github.com>
2021-09-15 09:59:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,6 @@ import streaming
 
 app = Flask(__name__)
 
-consumer_key = "UwNbJ8l6E6VrEi32y70yqun0Y"
-consumer_secret = "qt30g5cgZSls7hpcssYzym1GCmoGamliyUfUFQCroK8jh2nGYa"
-access_token = "1370678523468390401-TCDxmbNz1CjJsCIVNpMj4PIaBSRwQ5"
-access_token_secret = "g3kbIoJFvo6zDErOKMjzJWOXHit6BsXgKSyFZY8Lqf2bd"
-
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
